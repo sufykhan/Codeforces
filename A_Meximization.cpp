54fc568b7229d1d79aa2d4f0fee86b1132794c68
Almighty Stranger <sufyankhan9678@gmail.com>
2021-04-17 13:13:30 +0530

    Previous missed contest

--- A_Meximization.cpp
+++ A_Meximization.cpp
@@ -1,12 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,n) for(int i=a;i<n;i++)
-typedef long long intl;
-intl arr[101]={0};
 
-int main(){
-  intl t;cin>>t;
-  rep(ii,0,t){
-
-  }
+int main() {
+    int T;
+    cin >> T;
+    while (T --> 0) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        sort(a.begin(), a.end());
+        vector<int> b;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || a[i] != a[i - 1]) {
+                b.emplace_back(a[i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && a[i] == a[i - 1]) {
+                b.emplace_back(a[i]);
+            }
+        }
+        for (auto x : b) cout << x << ' ';
+        cout << '\n';
+    }
+    return 0;
+    cout << "amogus";
 }
