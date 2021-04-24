240e227a0bf9c850e6a4ef09a26bce0dc2100153
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-04-24 23:48:56 +0530

    Learnt about priority queue(Speeds up the implementation process otherwise i was really stuck)

--- D_Epic_Transformation.cpp
+++ D_Epic_Transformation.cpp
@@ -1,70 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,a,n) for(intl i=a;i<n;i++)
 typedef long long intl;
-intl t,n,x;
-int main(){
-  cin>>t;
-  rep(ii,0,t){
-      cin>>n;
-      vector<intl>v;
-      map<intl,intl>m;
-      rep(i,0,n){
-          cin>>x;
-          v.push_back(x);
-      }
-      rep(i,0,n){
-          if (m.find(v[i]) == m.end()) {
-            m[v[i]] = 1;
-        }
-        else {
-            m[v[i]]++;
-        }
-      }
-      intl arr[m.size()]={0},g=0;
-      for(auto& itr:m){
-          //cout<<"value-->"<<itr.second<<endl;
-         arr[g]=itr.second;
-          //cout<<arr[g]<<" ,";
-          g++;
-      }
-    //   rep(i,0,m.size()){
-    //       cout<<arr[i]<<" ,";
-    //   }
-       map<intl,intl>m1;
-       rep(i,0,m.size()){
-          if (m1.find(arr[i]) == m1.end()) {
-            m1[arr[i]] = 1;
-        }
-        else {
-            m1[arr[i]]++;
-        }
-      }
-      intl arr1[m.size()+1]={0},g1=0;
-      for(auto& itr:m1){
-          //cout<<"value-->"<<itr.second<<endl;
-          if(itr.second==1){
-              
-              arr1[g1]=itr.first;
-              //cout<<arr1[g1]<<"()";
-              g1++;
-          }
-          //cout<<arr[g]<<" ,";
-      }
-      intl sum=0;
-      for(intl i=1;i<g1;i++){
-          sum+=arr[i];
-        //   arr[i+1]=arr[i]-arr[i-1];
-         // arr1[i-1]=arr1[i]-arr1[i-1];
-      }
-      if(g1==0){
-          cout<<"0\n";
-      }
-      else{
-          cout<<arr[0]-sum<<endl;
-      }
-      //cout<<endl;
-    //   cout<<arr[g1-2]<<"\n";
-    //cout<<arr1[0]<<endl;
-  }
+typedef vector<intl> vi;
+typedef pair<intl, intl> pi;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, n) for (intl i = a; i < n; i++)
+
+void FASTIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
+
+bool isPrime(intl n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (intl i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+
+void print(vector<intl> v)
+{
+    for (auto x : v)
+    {
+        cout << x << ' ';
+    }
+    cout << endl;
+}
+
+bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
+{
+    return (a.second > b.second);
+}
+intl t, n;
+void solve(){
+    cin>>n;//cout<<n<<"\n";
+    vi v(n);map<intl,intl>m;priority_queue<intl>q;
+    rep(i,0,n) {cin>>v[i];m[v[i]]++;}
+    for(auto &x:m){
+        q.push(x.S);
+    }
+    while(q.size()>=2){
+        intl a=q.top();q.pop();
+        intl b=q.top();q.pop();
+        a--;b--;
+        if(a) q.push(a);
+        if(b) q.push(b);
+    }
+    if(q.size()==0) cout<<"0\n";
+    else cout<<q.top()<<"\n";
+    
+  
+}
+int main()
+{
+    FASTIO();
+    cin >> t;
+    rep(ii, 0, t)
+    {
+        solve();
+    }
+}
+//  1-2-3-1-2-3-1-2-3 -->oddcount*frequency%2==0->0 else 1; >1
+//                          evencount*frequency=0   >1
+//                          solo==1 -->arr[]={4,4,1,2,5,8}
+
+ 
