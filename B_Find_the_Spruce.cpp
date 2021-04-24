7c7f7b6634e86540ed1b27053aab8ed8001f9d24
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-04-25 01:50:52 +0530

    Getting comfortable in DP

--- B_Find_the_Spruce.cpp
+++ B_Find_the_Spruce.cpp
@@ -25,6 +25,29 @@ intl t, r, c;
 void solve()
 {
     cin >> r >> c;
+    intl dp[r][c];memset(dp,0,sizeof(dp));
+    rep(i,0,r){
+        string s;cin>>s;
+        rep(j,0,c){
+            if(s[j]=='*') dp[i][j]=1;
+        }
+    }
+    for(intl i=r-2;i>=0;i--){
+        for(intl j=0;j<c-2;j++){
+            if(dp[i+1][j]>0&&dp[i+1][j+1]>0&&dp[i+1][j+2]>0&&dp[i][j+1]>0){
+                dp[i][j+1]=min(min(dp[i+1][j],dp[i+1][j+1]),dp[i+1][j+2])+1;
+            }
+        }
+    }
+    intl ans=0;
+     rep(i,0,r){
+        rep(j,0,c){
+            ans+=dp[i][j];
+            //cout<<dp[i][j]<<" ";
+        }
+        //cout<<"\n";
+    }
+    cout<<ans<<"\n";
 }
 
 int main()
