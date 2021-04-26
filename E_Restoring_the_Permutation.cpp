8550bf8264a8f201b28bdab2637f4bde2c54aca8
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-04-26 22:12:07 +0530

    1500 read the problem incorrectly...now getting TLE

--- E_Restoring_the_Permutation.cpp
+++ E_Restoring_the_Permutation.cpp
@@ -47,53 +47,101 @@ bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
     return (a.second > b.second);
 }
 intl t, n;
-const intl N=2e5+5;
+const intl N = 2e5 + 5;
 intl cnt[N];
-void solve(){
-    cin>>n;vi v(n);rep(i,0,n) cin>>v[i];vi arr;vi nn;
-    vi arr1;vi arr2;
-    memset(cnt,0,sizeof(cnt));
-    arr.pb(v[0]);
-   arr1.pb(v[0]);
-    arr2.pb(v[0]);
-    nn.pb(v[0]);
-     cnt[arr[0]]++;
-    rep(i,0,n-1){
-        if(v[i]!=v[i+1]){
-            cnt[v[i+1]]++;
-            arr.pb(v[i+1]);
-            nn.pb(v[i+1]);
+
+void answer(vector<intl> arr, intl n, intl ind, vector<intl> arr2,vector<intl>v)
+{
+    rep(i, 1, n)
+    {
+        if (arr[i] == 0)
+        {
+            intl k = 1;
+            while (cnt[k] != 0)
+            {
+                k++;
+            }
+            cnt[k]++;
+            arr2.pb(k);
         }
-        else{
+        else
+        {
+            arr2.pb(arr[i]);
+        }
+    }
+   
+    print(arr2);
+    
+}
+
+void solve()
+{
+    cin >> n;
+    vi v(n);
+    rep(i, 0, n) cin >> v[i];
+    vi arr;
+    vi arr1;
+    vi arr2;
+    memset(cnt, 0, sizeof(cnt));
+    
+    arr.pb(v[0]);
+    arr1.pb(v[0]);
+    arr2.pb(v[0]);
+    cnt[arr[0]]++;
+    
+    rep(i, 0, n - 1)
+    {
+        if (v[i] != v[i + 1])
+        {
+            cnt[v[i + 1]]++;
+            
+            arr.pb(v[i + 1]);
+        }
+        else
+        {
             arr.pb(0);
         }
     }
-    print(arr);
-    bool flag=true;intl cnting=0;
-    rep(i,1,n){
-       if(cnting<nn.size()){
-           intl k=1;flag=true;
-           while(cnt[arr1[i-1]-k]!=0){
-               k++;
-               if(arr1[i-1]-k==0){
-                   flag=false;
-                   cnting++;
-                   break;
-               }
-           }
-           if(flag){
-           cnt[arr1[i-1]-k]++;
-           arr1.pb(arr1[i-1]-k);
-           }
-           else{
-            arr1.pb(nn[cnting]);
-            cnting++;
-           }
-       }
-       
-    }
+
+    //print(arr);
+
+   answer(arr,n, 1,arr2,v);
+
+    memset(cnt, 0, sizeof(cnt));
     
+    cnt[arr[0]]++;
+    
+    rep(i, 0, n - 1)
+    {
+        if (v[i] != v[i + 1])
+        {
+            cnt[v[i + 1]]++;
+            
+          
+        }
+    
+    }
+
+    rep(i, 1, n)
+    {
+        if (arr[i] == 0)
+        {
+            intl k = 1;
+            while (cnt[arr1[i - 1] - k] != 0)
+            {
+                k++;
+            }
+            cnt[arr1[i - 1] - k]++;
+            arr1.pb(arr1[i - 1] - k);
+        }
+        else
+        {
+
+            arr1.pb(arr[i]);
+        }
+    }
     print(arr1);
+
 }
 
 int main()
