0b70b92a7ec81d655e083148ba7f7641280c596a
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-04-29 00:32:23 +0530

    Target-->1300 ladder (5 Ques/day)

--- B_Nice_Matrix.cpp
+++ B_Nice_Matrix.cpp
@@ -1,73 +1,73 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// typedef long long intl;
-// typedef vector<intl> vi;
-// typedef pair<intl, intl> pi;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long intl;
+typedef vector<intl> vi;
+typedef pair<intl, intl> pi;
 
-// #define F first
-// #define S second
-// #define pb push_back
-// #define mp make_pair
-// #define rep(i, a, n) for (intl i = a; i < n; i++)
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, n) for (intl i = a; i < n; i++)
 
-// void FASTIO()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-// }
+void FASTIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
 
-// bool isPrime(intl n)
-// {
-//     if (n <= 1)
-//         return false;
-//     if (n <= 3)
-//         return true;
+bool isPrime(intl n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
 
-//     if (n % 2 == 0 || n % 3 == 0)
-//         return false;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
 
-//     for (intl i = 5; i * i <= n; i = i + 6)
-//         if (n % i == 0 || n % (i + 2) == 0)
-//             return false;
+    for (intl i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
 
-//     return true;
-// }
+    return true;
+}
 
-// void print(vector<intl> v)
-// {
-//     for (auto x : v)
-//     {
-//         cout << x << ' ';
-//     }
-//     cout << endl;
-// }
+void print(vector<intl> v)
+{
+    for (auto x : v)
+    {
+        cout << x << ' ';
+    }
+    cout << endl;
+}
 
-// bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
-// {
-//     return (a.second > b.second);
-// }
-// intl t, r,c;
+bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
+{
+    return (a.second > b.second);
+}
+intl t, r,c;
 
-// void solve(){
-//     cin>>r,c;intl arr[r][c];memset(arr,0,sizeof(arr));
-//     intl ans=0;
-//     rep(i,0,r){
-//         rep(j,0,c){
-//             cin>>arr[i][j];
+void solve(){
+    cin>>r,c;intl arr[r][c];memset(arr,0,sizeof(arr));
+    intl ans=0;
+    rep(i,0,r){
+        rep(j,0,c){
+            cin>>arr[i][j];
           
-//         }
-//     }
+        }
+    }
 
     
-// }
-// int main()
-// {
-//     FASTIO();
-//     cin >> t;
-//     rep(ii, 0, t)
-//     {
-//         solve();
-//     }
-// }
+}
+int main()
+{
+    FASTIO();
+    cin >> t;
+    rep(ii, 0, t)
+    {
+        solve();
+    }
+}
 
 
--- G_Years.cpp
+++ G_Years.cpp
@@ -1,95 +1,100 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// typedef long long intl;
-// typedef vector<intl> vi;
-// typedef pair<intl, intl> pi;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long intl;
+typedef vector<intl> vi;
+typedef pair<intl, intl> pi;
 
-// #define F first
-// #define S second
-// #define pb push_back
-// #define mp make_pair
-// #define rep(i, a, n) for (intl i = a; i < n; i++)
-// #define repr(i, a, n) for (intl i = a; i >=n; i--)
-// #define sort(v) sort(v.begin(),v.end())
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, n) for (intl i = a; i < n; i++)
+#define repr(i, a, n) for (intl i = a; i >=n; i--)
+#define sort(v) sort(v.begin(),v.end())
 
 
-// void FASTIO()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-// }
+void FASTIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
 
-// bool isPrime(intl n)
-// {
-//     if (n <= 1)
-//         return false;
-//     if (n <= 3)
-//         return true;
+bool isPrime(intl n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
 
-//     if (n % 2 == 0 || n % 3 == 0)
-//         return false;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
 
-//     for (intl i = 5; i * i <= n; i = i + 6)
-//         if (n % i == 0 || n % (i + 2) == 0)
-//             return false;
+    for (intl i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
 
-//     return true;
-// }
+    return true;
+}
 
-// void print(vector<intl> v)
-// {
-//     for (auto x : v)
-//     {
-//         cout << x << ' ';
-//     }
-//     cout << endl;
-// }
+void print(vector<intl> v)
+{
+    for (auto x : v)
+    {
+        cout << x << ' ';
+    }
+    cout << endl;
+}
 
-// bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
-// {
-//     return (a.second > b.second);
-// }
-// intl t, n;
+bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
+{
+    return (a.second > b.second);
+}
+intl t, n;
 
-// void solve(){
-//     cin>>n;vector<pair<intl,intl>>v;
-//     rep(i,0,n){
-//       intl x,y;cin>>x>>y;
-//       v.pb(mp(x,y-1));
-//     }
-//     sort(v);intl entry=v[0].F,exit=v[0].S; vector<vector<intl>>mega;
-//     // rep(i,0,n){
-//     //     cout<<v[i].F<<" "<<v[i].S<<"\n";
-//     // }
-//     // cout<<endl;
-//     intl ans=0;intl mx=1,startingmx=entry;
-//     for(int i=1;i<n;i++){
-//         if(v[i].F<=exit){
-//             entry=v[i].F;
-//             exit=v[i].S;
-//             ans++;
-//         }else{
-//             if(mx<ans+1){
-//                 startingmx=entry;mx=ans+1;
-//             }
-//         //    mega.pb({entry,exit,ans+1});
-//            ans=0;
-//            entry=v[i].F;
-//            exit=v[i].S;
-//         }
-//     }
-//     // rep(i,0,mega.size()){
-//     //     rep(j,0,3){
-//     //         cout<<mega[i][j]<<" ";
-//     //     }
-//     //     cout<<endl;
-//     // }
-//     cout<<endl;
-//    cout<<startingmx<<" "<<mx<<"\n";   
-// }
-// int main()
-// {
-//     FASTIO();
+void solve(){
+    cin>>n;vector<pair<intl,intl>>v;
+    rep(i,0,n){
+      intl x,y;cin>>x>>y;
+      v.pb(mp(x,y-1));
+    }
+    sort(v);intl entry=v[0].F,exit=v[0].S; vector<vector<intl>>mega;
+    // rep(i,0,n){
+    //     cout<<v[i].F<<" "<<v[i].S<<"\n";
+    // }
+    // cout<<endl;
+    intl ans=0;intl mx=1,startingmx=entry;
+    for(int i=1;i<n;i++){
+        if(v[i].F<=exit){
+            entry=v[i].F;
+            exit=v[i].S;
+            ans++;
+        }else{
+            if(mx<ans+1){
+                startingmx=entry;mx=ans+1;
+            }
+        //    mega.pb({entry,exit,ans+1});
+           ans=0;
+           entry=v[i].F;
+           exit=v[i].S;
+        }
+    }
+    if(ans>0){
+        if(mx<ans+1){
+            startingmx=entry;mx=ans+1;
+        }
+    }
+    // rep(i,0,mega.size()){
+    //     rep(j,0,3){
+    //         cout<<mega[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    cout<<endl;
+   cout<<startingmx<<" "<<mx<<"\n";   
+}
+int main()
+{
+    FASTIO();
     
-//         solve();
-// }
+        solve();
+}
