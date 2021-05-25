6fea7b2a00540574a973fce7fe04511c091bfcfd
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-05-25 07:35:02 +0530

    722 div 2 first time with dp on trees

---
+++ C_Parsa_s_Humongous_Tree.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long intl;
+typedef vector<intl> vi;
+typedef pair<intl, intl> pi;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, n) for (intl i = a; i < n; i++)
+
+void FASTIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+bool isPrime(intl n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (intl i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+
+void print(vector<intl> v)
+{
+    for (auto x : v)
+    {
+        cout << x << ' ';
+    }
+    cout << endl;
+}
+
+bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
+{
+    return (a.second > b.second);
+}
+intl t, n;
+
+void solve(){
+    cin>>n;
+    map<intl,pair<intl,intl>>mp;
+    rep(i,0,n){
+       intl x,y;cin>>x>>y;
+        mp[i+1]={x,y};
+    }
+    vector<intl>v[n];
+    rep(i,0,n-1){
+        intl x;cin>>x;
+        v[i+1].pb(x);
+    }
+ 
+}
+int main()
+{
+    FASTIO();
+    cin >> t;
+    rep(ii, 0, t)
+    {
+        solve();
+    }
+}
