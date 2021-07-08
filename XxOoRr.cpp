df75231185726e32839a38c00ae42c9a048a030c
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-07-08 10:10:30 +0530

    Upsolving

---
+++ XxOoRr.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long intl;
+typedef vector<intl> vi;
+typedef pair<intl, intl> pi;
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, n) for (intl i = a; i < n; i++)
+
+void FASTIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+bool isPrime(intl n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (intl i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+
+    return true;
+}
+
+void print(vector<intl> v)
+{
+    for (auto x : v)
+    {
+        cout << x << ' ';
+    }
+    cout << endl;
+}
+
+bool sortbySec(pair<intl, intl> &a, pair<intl, intl> &b)
+{
+    return (a.second > b.second);
+}
+intl t, n,k;
+
+void solve(){
+    cin>>n>>k;
+    vi v(n);
+    rep(i,0,n){
+        cin>>v[i];
+    }
+    intl setBit[32]={0};
+    for(auto x:v){
+        rep(i,0,32){
+           if(x&(1<<i))  setBit[i]+=1;
+        }
+    }
+    intl ans=0;
+    rep(i,0,32){
+        ans+=ceil((float)setBit[i]/k);
+    }
+    cout<<ans<<"\n";
+    
+}
+int main()
+{
+    FASTIO();
+    cin >> t;
+    rep(ii, 0, t)
+    {
+        solve();
+    }
+}
