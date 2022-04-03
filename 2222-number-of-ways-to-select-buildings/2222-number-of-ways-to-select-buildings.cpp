15882e40a1eb56e2fd45477d2f913b795938c707
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-04-03 13:19:36 +0530

    Time: 146 ms (12.50%), Space: 24.2 MB (12.50%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    long long cnt(string s1,string s2){
-        int n=s1.length(),m=s2.length();
-        long long dp[n+1][m+1];
-        
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0LL;
-        }
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1LL;
-        }
-    
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
-                }
+    long long numberOfWays(string s) {
+        int cnt0=0, cnt1=0;
+        for(char &i: s){
+            if(i=='1'){
+                cnt1++;
+            }
+            else{
+                cnt0++;
             }
         }
-        return dp[n][m];
-    }
-    long long numberOfWays(string s) {
-        long long ans=cnt(s,"101")+cnt(s,"010");
+        int prevcnt0=0, prevcnt1=0;
+        int long long ans=0;
+        for(char &i: s){
+            if(i=='1'){
+                ans+=prevcnt0*cnt0;
+                prevcnt1++;
+                cnt1--;
+            }
+            else{
+                ans+=prevcnt1*cnt1;
+                prevcnt0++;
+                cnt0--;
+            }
+        }
         return ans;
-        
     }
 };
