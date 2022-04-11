9de1bf5707222836e8ebfed3ac0f3201c6cd537f
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-04-11 14:13:02 +0530

    Time: 69 ms (81.82%), Space: 50.4 MB (11.85%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int maxReach=nums[0];
-        int n = nums.size();
+    bool canJump(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>dp(n+1,0);
+        dp[0]=arr[0];
+        
+        
         for(int i=1;i<n;i++){
-            if(maxReach == 0) return 0;
-            maxReach--;
-            if(nums[i] > maxReach) maxReach = nums[i];
+            
+            if(dp[i-1]<i){
+                return false;
+            }
+            dp[i]=max(dp[i-1],i+arr[i]);
+            
+            if(dp[i]>=n-1){
+                return true;
+            }
         }
-        return 1;
+        
+        return true;
+        
         
     }
 };
