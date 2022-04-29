6b1aa8b1488f0548146a994325d816b60adcf5ab
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-04-30 04:24:15 +0530

    Time: 3 ms (86.68%), Space: 8.9 MB (5.21%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,10 +4,15 @@ public:
          int l=0,r=nums.size()-1;
      
          int mid;
-        
+         if(r==0){
+             return 0;
+         }
+         if(r==1){
+             return nums[1]>nums[0];
+         }
          while(r>l){
              mid=l+(r-l)/2;
-             if(nums[mid]>nums[mid+1]){
+             if(mid+1<nums.size()&&nums[mid]>nums[mid+1]){
                  r=mid;
              }
              else{
