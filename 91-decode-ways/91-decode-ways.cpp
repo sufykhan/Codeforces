64e8c957964f9a924c9eeb476164ceae0f43c49e
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-05-09 16:22:18 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.57%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     
-    int recur(int i,string s,vector<int>&v){
-        if(s[i]=='0'){
-            return 0;
-        }
-        if(i>=s.size()-1){
-            return 1;
-        }
+//     int recur(int i,string s,vector<int>&v){
+//         if(s[i]=='0'){
+//             return 0;
+//         }
+//         if(i>=s.size()-1){
+//             return 1;
+//         }
       
-        if(v[i]!=-1){
-            return v[i];
-        }
-        string b=s.substr(i,2);
-        int way1=recur(i+1,s,v);
-        int way2=0;
-        if(stoi(b)>0 && stoi(b)<=26){
-            way2=recur(i+2,s,v);
-        }
-        return v[i]=way1+way2;
+//         if(v[i]!=-1){
+//             return v[i];
+//         }
+//         string b=s.substr(i,2);
+//         int way1=recur(i+1,s,v);
+//         int way2=0;
+//         if(stoi(b)>0 && stoi(b)<=26){
+//             way2=recur(i+2,s,v);
+//         }
+//         return v[i]=way1+way2;
         
-    }
+//     }
     int numDecodings(string s) {
             int ans=0;
             vector<int>dp(s.size()+1,-1);
-            // ans=recur(0,s,dp);
-
             dp[0]=1;
             if(s[0]=='0') dp[1]=0;
             else dp[1]=1;
