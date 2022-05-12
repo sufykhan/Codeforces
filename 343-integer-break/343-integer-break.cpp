183aaa83c3179fe52112e2475e05be080f8ef020
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-05-12 19:11:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (61.57%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,17 +1,48 @@
 class Solution {
 public:
+    int maxi=INT_MIN;
+    int ans=1;
+    int dp[60]={-1};
+    void recur(int n,int m){
+        
+        if(n<0){
+            return;
+        }
+        if(n==0){
+            cout<<ans<<" ";
+            maxi=max(maxi,ans);
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            if(i==m){
+                continue;
+            }
+            ans=ans*i;
+            recur(n-i,m);
+            ans=ans/i;
+        }
+    }
     int integerBreak(int n) {
-        if(n<=3) return n-1;
-        n++;
-        vector <int> dp(n+3,0);
-        dp[0] = 0;
-        dp[1] = 1;
+        
+        // int x=n;
+        // recur(x,x);
+        // return maxi;
+   
+        vector<int>dp(n+4,0);
+        dp[0]=0;
+        dp[1]=1;
         dp[2]=1;
-        for(int sq=3; sq<=n; sq++) {
-            for(int i=1; i<=sq; i++) {
-                dp[sq] = max(dp[sq], dp[sq-i]*i);
+        dp[3]=2;
+        if(dp[n]!=0) return dp[n];
+        
+        n++;
+        
+        for(int i=3;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]=max(dp[i],dp[i-j]*j);
             }
         }
         return dp[n];
+        
     }
 };
