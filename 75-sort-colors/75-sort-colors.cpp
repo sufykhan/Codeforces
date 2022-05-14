bc92dfc8216137d60396069e9ef717875d73fdad
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-05-14 23:36:07 +0530

    Time: 4 ms (41.85%), Space: 8.3 MB (25.30%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int i=0,j=arr.size()-1;
+        for(int k=0;k<=j&&j>=i;k++){
+            if(arr[k]==0){
+                swap(arr[k],arr[i]);
+                i++;
+            }
+            if(arr[k]==2){
+                swap(arr[k],arr[j]);
+                j--;
+                k--;
+            }
+        }
+        
+    }
+};
