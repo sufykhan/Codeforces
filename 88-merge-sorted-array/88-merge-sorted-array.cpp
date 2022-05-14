4956c8b856b3bf2bdfac5651bc3342b92598c635
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-05-15 02:53:51 +0530

    Time: 5 ms (31.22%), Space: 9.1 MB (73.18%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -5,22 +5,27 @@ public:
         int i=m-1,j=n-1;
         for(int k=m+n-1;k>=0;k--){
            
-            if(j>=0 && i>=0 &&nums2[j]>nums1[i]){
+            if(j>=0 && i>=0){
+                if( nums2[j]>nums1[i]){
                 nums1[k]=nums2[j];
-                j--;
+                    j--;
+                }
+                else if(j>=0 && i>=0 &&nums2[j]<=nums1[i]){
+                    nums1[k]=nums1[i];
+                    i--;
+                }
             }
-            else if(j>=0 && i>=0 &&nums2[j]<=nums1[i]){
-                nums1[k]=nums1[i];
-                i--;
+            else{
+                if(j>=0){
+                    nums1[k]=nums2[j];
+                    j--;
+                }
+                else if(i>=0){
+                    nums1[k]=nums1[i];
+                    i--;
+                }
             }
-            else if(i<0 && j>=0){
-                nums1[k]=nums2[j];
-                j--;
-            }
-            else if(j<0 && i>=0){
-                nums1[k]=nums1[i];
-                i--;
-            }
+        
         }
         
     }
