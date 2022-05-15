80347e2d44681acc1516a1feaf5f058a93244934
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-05-15 15:24:54 +0530

    Time: 5 ms (25.85%), Space: 5.8 MB (95.01%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    // bool check(int x){
-    //      if(x==1 ||x==10|| x==100 || x==68 || x==86 || x==28 || x==82 || x==19 || x==91||x==13||x==31||x==23||x==32)          {
-    //         return true;
-    //      }
-    //      return false;
-    // }
-    // int cnt(int n){
-    //     string s=to_string(n);
-    //     return s.length();
-    // }
+    int go(int n){
+        int ans=0;
+        while(n!=0){
+            ans+=(n%10)*(n%10);
+            n=n/10;
+        }
+        return ans;
+    }
     bool isHappy(int n) {
-         map<int,int>mp;
-         
-         while(mp[n]<=1){
-             mp[n]++;
-             string s=to_string(n);
-             int num=0;
-             for(int i=0;i<s.length();i++){
-                 num+=(s[i]-'0')*(s[i]-'0');
+         int fast=n,slow=n;
+         do{
+             fast=go(go(fast));
+             slow=go(slow);
+             if(fast==1){
+                 return true;
              }
-             n=num;
-             if(n==1) return true;
-         }
+         } while(fast!=slow);
          return false;
+         
     }
 };
