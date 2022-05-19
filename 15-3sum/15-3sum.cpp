ac044ded4039587104ca25519a4e2a4e2d48c927
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-05-19 23:23:08 +0530

    Time: 109 ms (63.18%), Space: 19.8 MB (99.87%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,60 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
-        
-        map<int,int>mp;
-        
-        for(auto x:arr){
-            mp[x]++;
-        }
-        
-        sort(arr.begin(),arr.end());
-        vector<int>::iterator ip;
-        ip = unique(arr.begin(), arr.end());
-        arr.resize(distance(arr.begin(), ip));
-      
-        for(auto x:arr){
-            cout<<x<<" ";
-        }
-        cout<<"\n";
         int n=arr.size();
-        
+        if(n<3) return {};
+        sort(arr.begin(),arr.end());
         vector<vector<int>>ans;
-        
         for(int i=0;i<n;i++){
-           
-            
             int target=-1*arr[i];
-             cout<<target<<" \n";
-            
-            int j=i+1,k=n-1;
-            while(k>j){
-                if(arr[j]+arr[k]==target){
-                    ans.push_back({arr[j],arr[k],arr[i]});
-                    j++;
-                    k--;
+            int l=i+1,r=n-1;
+            while(r>l){
+                if(arr[l]+arr[r]==target){
+                    ans.push_back({arr[i],arr[l],arr[r]});
+                    while(r>l&&arr[l]==ans.back()[1]){
+                        l++;
+                    }
+                    while(r>l&&arr[r]==ans.back()[2]){
+                        r--;
+                    }
                 }
-                else if (arr[j]+arr[k]<target){
-                    j++;
+                else if (arr[l]+arr[r]>target){
+                    r--;
                 }
                 else{
-                    k--;
+                    l++;
                 }
+            }
+            while(i+1<n&&arr[i]==arr[i+1]){
+                i++;
             }
             
-            j=0;
-            while(j<n){
-                if(i!=j && 2*arr[j]==target && mp[arr[j]]>=2){
-                     ans.push_back({arr[j],arr[j],arr[i]});
-                }
-                j++;
-                
-            }
         }
-        if(mp[0]>=3){
-            ans.push_back({0,0,0});
-        }
-        
         return ans;
+        
     }
 };
