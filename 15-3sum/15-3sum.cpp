7cde265bdccbc520c4f6ba3f444d5ea52e7f0d40
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-04 13:07:20 +0530

    Time: 104 ms (70.21%), Space: 20.1 MB (68.40%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,32 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
         int n=arr.size();
         if(n<3) return {};
-        sort(arr.begin(),arr.end());
+        if(arr[0]>0) return {};
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
+            if(arr[i]>0){
+                break;
+            }
+            if(i>0 && arr[i]==arr[i-1]){
+                continue;
+            }
             int target=-1*arr[i];
+            //cout<<i<<"<--> "<<target<<"--->";
             int l=i+1,r=n-1;
             while(r>l){
+               // cout<<arr[l]+arr[r]<<" \n";
                 if(arr[l]+arr[r]==target){
                     ans.push_back({arr[i],arr[l],arr[r]});
-                    while(r>l&&arr[l]==ans.back()[1]){
+                    int x=arr[l];
+                    int y=arr[r];
+                    while(r>l && arr[l]==x){
                         l++;
                     }
-                    while(r>l&&arr[r]==ans.back()[2]){
+                    while(r>l && arr[r]==y){
                         r--;
                     }
                 }
-                else if (arr[l]+arr[r]>target){
-                    r--;
-                }
-                else{
+                else if(arr[l]+arr[r]<target){
                     l++;
                 }
-            }
-            while(i+1<n&&arr[i]==arr[i+1]){
-                i++;
+                else{
+                    r--;
+                }
             }
             
         }
