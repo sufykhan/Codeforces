0389e643648b31eb57ea9c71e4f4199028531fb3
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-04 15:15:06 +0530

    Time: 15 ms (77.32%), Space: 19.8 MB (26.50%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
+    int value(vector<int>& height){
+        int great=height[0];
+        int ans=0,cnt=0,sum=0;
+        for(auto x:height){
+           if(great>=x){
+               cnt++;
+               ans-=x;
+               sum+=x;
+           }
+           else{
+               ans+=cnt*great;
+               cnt=0;
+               sum=0;
+               great=x;
+           }
+        }
+        if(cnt!=0) ans+=sum;
+        return ans;
+    }
     int trap(vector<int>& height) {
-        if(height.empty())
-            return 0;
-        int ans = 0;
-        int size = height.size();
-        vector<int> left_max(size), right_max(size);
-        left_max[0] = height[0];
-        for (int i = 1; i < size; i++) {
-            left_max[i] = max(height[i], left_max[i - 1]);
+        int ans=0;
+        ans+=value(height);
+        reverse(height.begin(),height.end());
+        ans+=value(height);
+        
+        int maxi=*max_element(height.begin(),height.end());
+      
+        int pos1=-1,pos2=-1;
+        for(int i=0;i<height.size();i++){
+            if(maxi==height[i]){
+                if(pos1==-1) pos1=i;
+                pos2=i;
+            }
+        
         }
-        right_max[size - 1] = height[size - 1];
-        for (int i = size - 2; i >= 0; i--) {
-            right_max[i] = max(height[i], right_max[i + 1]);
-        }
-        for (int i = 1; i < size - 1; i++) {
-            ans += min(left_max[i], right_max[i]) - height[i];
+       // cout<<pos1<<" "<<pos2<<" ";
+        for(int i=pos1;i<=pos2;i++){
+            ans+=maxi-height[i];
         }
         return ans;
-        }
+    }
 };
