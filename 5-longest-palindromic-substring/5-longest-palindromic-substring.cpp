3a136e0207c245e6c8531d3a6588edecae2e91c7
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-10 01:18:29 +0530

    Time: 1359 ms (5.37%), Space: 383.9 MB (11.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,56 +1,59 @@
 class Solution {
 public:
-    vector<int> check(string s,int pivot){
-        int dans=0;
-        vector<int>ans(3,0);
-        for(int k=1;k<=2;k++){
-            int d1=k;
-            if(k==2 && (pivot+1>=s.size() || s[pivot]!=s[pivot+1])){
-                break;
-            }
-            int i=pivot-1,j=pivot+k;
-            while(i>=0 && j<s.size()&&s[i]==s[j]){
-               
-                    d1+=2;
-                    i--;
-                    j++;
-                
-               
-            }
-            if(d1>dans){
-                
-                dans=d1;
-                ans[0]=dans;
-                ans[1]=i+1;
-                ans[2]=j-1;
-            }
-        }
-        return ans;
-        
-    }
-    
     string longestPalindrome(string s) {
-        int ans=0,i=0,j=0;
         
-//         vector<int>val=check(s,0);
-//         cout<<val[0]<<" "<<val[1]<<" "<<val[2];
-        
-      //  return "";
-        for(int i1=0;i1<s.size();i1++){
-            cout<<i1<<" ";
-            vector<int>val=check(s,i1);
-            if(val[0]>ans){
-                ans=val[0];
-                i=val[1];
-                j=val[2];
+        int n=s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int ans=1,posi,posj;
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+            if(i+1<n&&s[i]==s[i+1]){
+                dp[i][i+1]=2;
+                ans=2;
+                posi=i,posj=i+1;
             }
-            
         }
-        cout<<ans<<" ";
+        for(int size=3;size<=n;size++){
+            for(int i=0;i+size<=n ;i++){
+                int j=size+i-1;
+                
+                if(s[i]==s[j] && dp[i+1][j-1]!=0){
+                    dp[i][j]=2+dp[i+1][j-1];
+                    
+                }
+                else{
+                    dp[i][j]=0;
+                }
+                
+                ans=max(ans,dp[i][j]);
+                if(ans==dp[i][j]) posi=i,posj=j;
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        // cout<<ans<<" ";
         string str="";
-        for(int k=i;k<=j;k++){
-            str+=s[k];
+        if(ans==1){
+            str+=s[0];
+            return str;
         }
+        while(posi<n && posj>=0 &&dp[posi][posj]!=0){
+            str+=s[posi];
+            posi++;
+            posj--;
+        }
+        
+        string str2=str;
+        reverse(str2.begin(),str2.end());
+        if(ans%2==1){
+            str.pop_back();
+        }
+        str=str+str2;
         return str;
+        
     }
 };
