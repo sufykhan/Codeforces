e777b5b38f9b2e69ca86dcb9ceeebca9af6c2951
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-11 01:47:33 +0530

    Time: 3 ms (42.62%), Space: 6.4 MB (22.73%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,9 +1,40 @@
 class Solution {
 public:
+    void computeLPS(string s,vector<int>&pref){
+        for(int i=1;i<s.size();i++){
+            int j=pref[i-1];
+            while(j>0&&s[i]!=s[j]){
+                j=pref[j-1];
+            }
+            if(s[i]==s[j]){
+                j++;
+            }
+            pref[i]=j;
+        }
+    }
     int strStr(string s, string n) {
-        size_t val = s.find(n);
-        if(val!=string::npos){
-            return val;
+        
+        vector<int>pref(n.size(),0);
+        computeLPS(n,pref);
+        
+        int i=0,j=0;
+        while(i<s.size()){
+            if(s[i]==n[j]){
+                i++;
+                j++;
+            }
+            if(j==n.size()){
+                return i-j;
+            }
+            
+            if(i<s.size() && s[i]!=n[j]){
+                if(j!=0){
+                    j=pref[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
         }
         return -1;
     }
