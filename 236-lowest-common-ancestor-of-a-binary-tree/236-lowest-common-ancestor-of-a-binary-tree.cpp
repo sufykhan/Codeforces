16c277b371146d521d32ad31b370cbbd14caa1a6
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-13 18:33:17 +0530

    Time: 37 ms (11.83%), Space: 17.8 MB (5.05%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* ans;
+    bool recur(TreeNode*root,map<TreeNode*,bool>&mp,TreeNode*p){
+        
+        if(root==nullptr){
+            return false;
+        }
+        if(root==p){
+            mp[root]=true;
+            return true;
+        }
+        bool ans1=recur(root->left,mp,p) || recur(root->right,mp,p);
+        if(ans1){
+            mp[root]=true;
+        }
+        return ans1;
+    }
+    
+    
+    bool recur2(TreeNode*root,map<TreeNode*,bool>&mp,TreeNode*p){
+        if(root==nullptr){
+            return false;
+        }
+        if(root==p){
+            if(mp.find(root)!=mp.end() && !ans){
+                ans=root;
+            }
+        
+            return true;
+        }
+
+        bool ans1=recur2(root->left,mp,p) || recur2(root->right,mp,p);
+        if(ans1){
+            if(mp.find(root)!=mp.end() && !ans){
+                ans=root;
+            }
+        }
+        return ans1;
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        map<TreeNode* ,bool>mp;
+       
+        recur(root,mp,p);
+       recur2(root,mp,q);
+        return ans;
+    }
+};
