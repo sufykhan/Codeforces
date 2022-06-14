9356714b7fde0dd1e63e2655738be6aea03e542c
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-14 17:09:59 +0530

    Time: 1088 ms (5.02%), Space: 380.2 MB (5.02%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
+    int recur(string s1,string s2,int i,int j,vector<vector<int>>&dp){
+        if(i<=0 || j<=0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i-1]==s2[j-1]){
+            return dp[i][j]=1+recur(s1,s2,i-1,j-1,dp);
+        }
+        else{
+            return dp[i][j]=max(recur(s1,s2,i-1,j,dp),recur(s1,s2,i,j-1,dp));
+        }
+        
+        
+    }
     int minDistance(string s1, string s2) {
-        
-        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
-        
-        for(int i=0;i<=s1.size();i++){
-            dp[i][0]=i;
-        }
-        for(int i=0;i<=s2.size();i++){
-            dp[0][i]=i;
-        }
-        
-        for(int i=1;i<=s1.size();i++){
-            for(int j=1;j<=s2.size();j++){
-                
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
-                }
-                
-            }
-        }
-        
-        return dp[s1.size()][s2.size()];
+            vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+            int ans=recur(s1,s2,s1.size(),s2.size(),dp);
+            ans=s1.length()+s2.length()-2*ans;
+            return ans;
     }
 };
