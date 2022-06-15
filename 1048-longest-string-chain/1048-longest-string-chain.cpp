791deac03d772446f47100f2bea1d952318b48f9
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-16 01:35:54 +0530

    Time: 121 ms (64.49%), Space: 26.6 MB (23.39%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    static bool compare(const string &s1, const string &s2) {
-        return s1.length() < s2.length();
-    }
-
-    int longestStrChain(vector<string>& words) {
-        sort(words.begin(), words.end(), compare);
-        unordered_map<string, int> dp;
-        int res = 0;
-        for (string w : words) {
-            for (int i = 0; i < w.length(); i++) {
-                string pre = w.substr(0, i) + w.substr(i + 1);
-                dp[w] = max(dp[w], dp.find(pre) == dp.end() ? 1 : dp[pre] + 1);
+    static bool check(string s1,string s2){
+        if(s2.size()-s1.size()==1){
+            int i=0,j=0,cnt=0;
+            while(i<s1.size()&&j<s2.size()){
+                if(s1[i]==s2[j]){
+                    i++;
+                    j++;
+                }
+                else{
+                    j++;
+                    cnt++;
+                }
             }
-            res = max(res, dp[w]);
+            return cnt<2;
         }
-        return res;
+        return false;
+    } 
+    
+    int longestStrChain(vector<string>& s) {
+        
+        int ans=INT_MIN;
+        sort(s.begin(),s.end(),[](string a,string b){return a.size()<b.size();});
+        int n=s.size();
+        //vector<int>dp(n,1);
+        unordered_map<string,int>dp;
+    
+        string s1;
+        for(auto &str:s){
+            dp[str]=1;
+            for(int i=0;i<str.size();i++){
+                s1=str.substr(0,i)+str.substr(i+1);
+                if(dp.find(s1)!=dp.end()){
+                    dp[str]=max(dp[str],1+dp[s1]);
+                }
+            }
+        
+        }
+        for(int i=0;i<n;i++){
+            ans=max(ans,dp[s[i]]);
+        }
+        return ans;
+        
     }
 };
