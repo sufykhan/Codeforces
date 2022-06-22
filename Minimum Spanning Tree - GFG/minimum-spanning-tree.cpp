2d54f56b03bfca08690dcb5e388fc54e801cf40d
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-22 11:04:03 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,27 +9,55 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
+    int find(int x,vector<int>&parent){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x]=find(parent[x],parent);
+    }
+    
+    void Union(int x,int y,vector<int>&rank,vector<int>&parent){
+        int xp=find(x,parent);
+        int yp=find(y,parent);
+      
+        if(rank[xp]>rank[yp]){
+            parent[yp]=xp;
+        }
+        else if(rank[xp]<rank[yp]){
+            parent[xp]=yp;
+        }
+        else{
+            parent[xp]=yp;
+            rank[yp]++;
+        }
+    }
+    int spanningTree(int V, vector<vector<int>> v[])
     {
-        vector<int>dist(V,INT_MAX);
-        vector<int>vis(V,false);
-        
-        dist[0]=0;
-        for(int k=0;k<V-1;k++){
-            int mini=INT_MAX,pos=0;
-            for(int i=0;i<V;i++){
-                if(!vis[i]&&mini>=dist[i]){
-                    pos=i;
-                    mini=dist[i];
-                }
-            }
-            vis[pos]=true;
-            
-            for(auto x:adj[pos]){
-               if(!vis[x[0]]) dist[x[0]]=min(dist[x[0]],x[1]);
+        vector<vector<int>>adj;
+        for(int i=0;i<V;i++){
+            for(auto &x:v[i]){
+                adj.push_back({i,x[0],x[1]});
             }
         }
-        return accumulate(dist.begin(),dist.end(),0);
+       sort(adj.begin(),adj.end(),[](const vector<int>&a,const vector<int>&b){ return a[2]<b[2];});
+       
+       int ans=0;
+       vector<int>parent(V);
+       vector<int>rank(V);
+       for(int i=0;i<V;i++){
+           parent[i]=i;
+           rank[i]=1;
+       }
+       
+       for(auto &x:adj){
+           
+           if(find(x[1],parent)!=find(x[0],parent)){
+               Union(x[0],x[1],rank,parent);
+               ans+=x[2];
+           }
+           
+       }
+       return ans;
     }
 };
 
