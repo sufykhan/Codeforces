31dacefac87b009fb6de860169b439c7fbad1604
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-06-23 00:21:05 +0530

    Time: 440 ms (12.67%), Space: 13.1 MB (81.05%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -11,55 +11,22 @@
 
 class Solution {
 public:
-    ListNode* merge(ListNode* l11, ListNode* l22) {
-        
-        if(l11==nullptr) return l22;
-        if(l22==nullptr) return l11;
-        
-        if(l11->val<l22->val){
-            swap(l11,l22);
-        }
-        
-        ListNode* l1=l11;
-        ListNode* l2=l22;
-        ListNode* prevl2;
-        ListNode* prevl1;
-        
-        while(l1!=nullptr && l2!=nullptr){
-            
-            if(l1!=nullptr && l2!=nullptr && l1->val>=l2->val){
-                while(l1!=nullptr && l2!=nullptr && l1->val>=l2->val){
-                    prevl2=l2;
-                    l2=l2->next;
-                }
-                prevl2->next=l1;
-            }
-            else{
-                while(l1!=nullptr && l2!=nullptr &&l2->val>=l1->val){
-                    prevl1=l1;
-                    l1=l1->next;
-                }
-                prevl1->next=l2;
-            }
-       }
-       return l22; 
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
     }
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* root;
-        int n=lists.size();
-        if(n==0) return nullptr;
-        int idx=1;
+        if (lists.size() == 0) return NULL;
         
-        ListNode* s1=lists[0];
-        ListNode* s2;
-    
-        while(idx<n){
-                s2=lists[idx];
-                s1=merge(s1,s2);
-                idx++;
-        }
-        return s1;
+        ListNode* head = lists[0];
         
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
     }
 };
