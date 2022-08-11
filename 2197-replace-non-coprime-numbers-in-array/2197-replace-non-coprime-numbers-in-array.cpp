7785199cdeb618c26281ead145fee2dced6f326a
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-08-11 07:00:29 +0530

    Time: 273 ms (80.74%), Space: 120.9 MB (72.82%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
-    vector<int> check(vector<int>nums){
-        vector<int>ans;
-        int prev=nums[0];
-        nums.push_back(INT_MAX);
-        for(int i=1;i<nums.size();i++){
-              int gcd=__gcd(nums[i],prev);
-              if(gcd==1){
-                  ans.push_back(prev);
-                  prev=nums[i];
-              }
-              else{
-                  prev=(nums[i]*1LL*prev)/gcd;
-              }
-        }
-        return ans;
-    }
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        
-        vector<int>x1=check(nums);
-        reverse(nums.begin(),nums.end());
-        vector<int>x11=check(nums);
-        bool flg=false;
-        if(x11.size()<x1.size()){
-            flg=true;
-            x1=x11;
+        vector<int>res;
+        for(auto &x:nums){
+            while(1){
+                int gcd=__gcd(res.size()==0?1:res.back(),x);
+                if(gcd==1) break;
+                x=(x*1LL*res.back())/gcd;
+                res.pop_back();
+            }
+            res.push_back(x);
         }
-        vector<int>x2=check(x1);
-        while(x1!=x2){
-            x1=x2;
-            x2=check(x1);
-        }
-        if(flg) reverse(x1.begin(),x1.end());
-        return x1;
-        
-        
+        return res;
     }
 };
