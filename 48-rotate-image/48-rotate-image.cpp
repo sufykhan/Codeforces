faabc626bb7a5406e46cf2dd2d50bf1666d28a70
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-08-30 18:51:00 +0530

    Time: 7 ms (17.34%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-       
-        for(int i=0;i<(n+1)/2;i++){
-            for(int j=0;j<n/2;j++){
-                
-                int x=i,y=j;int temp=matrix[x][y];
-                for(int k=0;k<3;k++){
-                    matrix[x][y]=matrix[n-y-1][x];
-                    int tx=x,ty=y;
-                    y=tx;
-                    x=n-ty-1;
-                }
-                matrix[x][y]=temp;
-               
+    
+    void transpose(vector<vector<int>>&mat){
+        for(int i=0;i<mat.size();i++){
+            for(int j=i+1;j<mat.size();j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
             }
         }
+    }
+    void revert(vector<vector<int>>&mat){
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat.size()/2;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[i][mat.size()-j-1];
+                mat[i][mat.size()-j-1]=temp;
+            }
+        }
+    }
+    void recur(vector<vector<int>>&mat){
+        transpose(mat);
+        revert(mat);
+    }
+    void rotate(vector<vector<int>>& matrix) {
+        recur(matrix);
     }
 };
