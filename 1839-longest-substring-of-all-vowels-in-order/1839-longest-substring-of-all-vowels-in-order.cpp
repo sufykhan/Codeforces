26d1f18a08418806198e7789403ab333592610fa
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-09-08 04:02:09 +0530

    Time: 154 ms (56.75%), Space: 26.9 MB (36.79%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -1,45 +1,17 @@
 class Solution {
 public:
     int longestBeautifulSubstring(string word) {
-       map<char,int>mp;
-       mp['a']=1;
-        mp['e']=2;
-        mp['i']=3;
-        mp['o']=4;
-        mp['u']=5;
-        
-        vector<int>ans;
-        for(auto x:word) ans.push_back(mp[x]);
-        
-        vector<pair<int,int>>v;
-        
-        int cnt=0;
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]==ans[i-1]){
-                cnt++;
-            }
-            else{
-                v.push_back({ans[i-1],cnt+1});
-                cnt=0;
-            }
-        }
-       if(v.size()!=0 && v.back().first!=ans.back()) v.push_back({ans[word.size()-1],cnt+1});
-        
-      // for(auto x:v) cout<<x.first<<" "<<x.second<<"\n";
-        
         int maxi=0;
-        for(int i=0;i<v.size();i++){
-            if(v[i].first==1){
-                int j=2;
-                int cnt1=v[i].second;
-                while(i+j-1<v.size() && v[i+j-1].first==j){
-                    cnt1+=v[i+j-1].second;
-                    j++;
+        for(int i=0,j=0;i<word.size();i++){
+            if(word[i]=='a'){
+                int cnt=0;
+                for(j=i+1;j<word.size() && word[j-1]<=word[j];j++){
+                    cnt+=word[j-1]<word[j];
                 }
-                i=i+j-1;
-                i--;
-                if(j==6) maxi=max(cnt1,maxi);
+                if(cnt==4) maxi=max(maxi,j-i);
+                i=j-1;
             }
+          
         }
         return maxi;
     }
