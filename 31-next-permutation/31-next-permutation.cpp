13af104a66891ea88b142d632d177e463ecbd5ce
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-09-15 00:03:34 +0530

    Time: 13 ms (13.67%), Space: 12.2 MB (36.51%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>&arr) {
-        int pos=-1,n=arr.size();
-        for(int i=n-2;i>=0;i--){
-            if(arr[i]<arr[i+1]){
-                pos=i;
-                break;
-            }
-        }
-      
-        reverse(arr.begin()+pos+1,arr.end());
-        if(pos==-1){
-            return;
-        }
-        for(int i=pos+1;i<n;i++){
-            if(arr[i]>arr[pos]){
-                swap(arr[i],arr[pos]);
+    void nextPermutation(vector<int>& nums) {
+        
+        // 1 2 5 4 1
+        // 5>2 => 2 need to displace with next greater element (4)
+        // 1 4|(5 2 1) => reverse bracket part to get smallest value after swapped element(4)
+        // 1 4 1 2 5 => Ans
+        
+        int n=nums.size();
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                int pos=i;
+                for(int j=i+1;j<n;j++){
+                    if(nums[j]>nums[i-1]) pos=j;
+                }
+                swap(nums[pos],nums[i-1]);
+                reverse(nums.begin()+i,nums.end());
                 return;
             }
         }
+        reverse(nums.begin(),nums.end());
+        
     }
 };
