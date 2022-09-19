940eb2f9f3c537313fe2fa1f752a9e2f2ca28227
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-09-20 03:57:49 +0530

    Time: 12 ms (15.72%), Space: 11 MB (74.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,51 +1,42 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        
-        int l=0,r=arr.size();
-        int center=0;
-        while(r>l){
+    int binSearch(vector<int>&nums,int target,int l,int r){
+        while(r>=l){
             int mid=l+(r-l)/2;
-            if(arr[mid]==target){
+            if(nums[mid]==target){
                 return mid;
             }
-            if(mid>=1 && arr[mid]<arr[mid-1]){
-                center=mid;
-                break;
-            }
-            else if(mid+1<arr.size() && arr[mid]>arr[mid+1]){
-                center=mid;
-                break;
-            }
-            else if(arr[mid]>arr[l]){
-                l=mid+1;
+            if(nums[mid]>target){
+                r=mid-1;
             }
             else{
-                r=mid;
-            }
-        }
-        
-        if(target>arr.back()){
-            l=0,r=center;
-        }
-        else{
-            l=center,r=arr.size();
-        }
-        
-        while(r>l){
-            int mid=l+(r-l)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            else if(arr[mid]>target){
-                r=mid;
-            }
-            else if(arr[mid]<target){
                 l=mid+1;
             }
         }
         return -1;
-        
+    }
+    int search(vector<int>& nums, int target) {
+        int pivot=-1;
+        int l=0,r=nums.size()-1;
+        while(r>l){
+            int mid=l+(r-l)/2;
+            if(mid+1!=nums.size() && nums[mid]>nums[mid+1]){
+                pivot=mid;
+                break;
+            }
+            else if(nums[r]>nums[mid]){
+                r=mid;
+            }
+            else{
+                l=mid;
+            }
+        }
+       
+        int ans1=binSearch(nums,target,0,pivot);
+        if(ans1!=-1) return ans1;
+        int ans2=binSearch(nums,target,pivot+1,nums.size()-1);
+        if(ans2!=-1) return ans2;
+        return -1;
         
     }
 };
