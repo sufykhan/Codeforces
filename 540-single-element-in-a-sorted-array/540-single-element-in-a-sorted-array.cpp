79b400870aa130e93bf5939e68e4dbefbf6223ce
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-09-20 03:24:19 +0530

    Time: 60 ms (20.60%), Space: 22.3 MB (91.45%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>&arr) {
-
-        int l=0,r=arr.size();
-        
-        if((r>1 && arr[0]!=arr[1]) || r==1){
-            return arr[0];
-        }
-        if(r>=2&&arr[r-1]!=arr[r-2]){
-            return arr[r-1];
-        }
-        while(r>l){
+    int singleNonDuplicate(vector<int>& nums) {
+        // 1 1 2 3 3 4 4 8 8
+        // 0 1 2 3 4 5 6 7 8    
+        int l=0,r=nums.size()-2;
+        while(r>=l){
             int mid=l+(r-l)/2;
-            if(mid>0 && arr[mid]==arr[mid-1]){
-                if(mid%2==0){
+            if(mid%2==0){
+                if(nums[mid]!=nums[mid+1]){
                     r=mid-1;
                 }
                 else{
                     l=mid+1;
                 }
             }
-            else if(mid+1<arr.size() && arr[mid]==arr[mid+1]){
-                if((mid+1)%2==0){
-                    r=mid;
+            else{
+                if(nums[mid]==nums[mid+1]){
+                    r=mid-1;
                 }
                 else{
-                    l=mid+2;
+                    l=mid+1;
                 }
             }
-            else if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
-                return arr[mid];
-            }
         }
-        return -1;
+        return nums[l];
     }
 };
