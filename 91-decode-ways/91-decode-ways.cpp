8bd3c3b5d667d494f6ab3faa3b9475e5e5618810
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-10-02 00:40:41 +0530

    Time: 5 ms (33.84%), Space: 6 MB (93.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,40 +1,26 @@
+
 class Solution {
 public:
     
-//     int recur(int i,string s,vector<int>&v){
-//         if(s[i]=='0'){
-//             return 0;
-//         }
-//         if(i>=s.size()-1){
-//             return 1;
-//         }
-      
-//         if(v[i]!=-1){
-//             return v[i];
-//         }
-//         string b=s.substr(i,2);
-//         int way1=recur(i+1,s,v);
-//         int way2=0;
-//         if(stoi(b)>0 && stoi(b)<=26){
-//             way2=recur(i+2,s,v);
-//         }
-//         return v[i]=way1+way2;
-        
-//     }
     int numDecodings(string s) {
             int ans=0;
+            
             vector<int>dp(s.size()+1,-1);
-            dp[0]=1;
-            if(s[0]=='0') dp[1]=0;
+        
+            dp[0]=1;  //No. of way to decode empty string
+            
+            if(s[0]=='0') dp[1]=0;   //Not a valid case
             else dp[1]=1;
             
-            for(int i=2;i<s.size()+1;i++){
+            for(int i=2;i<=s.size();i++){
                 
-                if(s[i-1]=='0') dp[i]=0;
-                else dp[i]=dp[i-1];
-                string b=s.substr(i-2,2);
+                if(s[i-1]=='0') dp[i]=0;            //To handle the case that starts with '0'
+                else dp[i]=dp[i-1];                  
+                
+                string b=s.substr(i-2,2);           // Gets value of last 2 digit ending at i;
+                
                 if(stoi(b)>0 && stoi(b)<=26 && s[i-2]!='0'){
-                    dp[i]+=dp[i-2];
+                    dp[i]+=dp[i-2];                 //As we are checking pair wise 
                 }
                 
                 
