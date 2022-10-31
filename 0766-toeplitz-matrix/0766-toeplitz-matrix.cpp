bf5c21d6ae5d0214c7a60eb14dc034d8caacd6a7
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-11-01 02:43:32 +0530

    Time: 24 ms (72.94%), Space: 17.5 MB (63.89%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  
+    static bool isToeplitzMatrix(const vector<vector<int>>& matrix) {
+        const int rows = matrix.size();
+        const int cols = matrix[0].size();
+        for (int r = 1; r < rows; ++r)
+            for (int c = 1; c < cols; ++c)
+                if (matrix[r][c] != matrix[r - 1][c - 1]) return false;
+        
+        return true;
+    }
+};
