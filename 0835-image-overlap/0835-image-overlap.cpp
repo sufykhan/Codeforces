2e4ce91b352bb624820493500fcb361eae69fafa
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-11-03 02:11:51 +0530

    Time: 365 ms (29.85%), Space: 9.9 MB (73.63%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
-    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<int> LA, LB;
-        int N = A.size();
-        unordered_map<int, int> count;
-        for (int i = 0; i < N * N; ++i)
-            if (A[i / N][i % N] == 1)
-                LA.push_back(i / N * 100 + i % N);
-        for (int i = 0; i < N * N; ++i)
-            if (B[i / N][i % N] == 1)
-                LB.push_back(i / N * 100 + i % N);
-        for (int i : LA) for (int j : LB) count[i - j]++;
-        int res = 0;
-        for (auto it : count) res = max(res, it.second);
-        return res;
+    int comp(vector<vector<int>>& img1, vector<vector<int>>& img2){
+        int n=img1.size();
+        vector<vector<int>>grid(3*n-2,vector<int>(3*n-2,0));
+        int maxi=0;
+        for(int i=n-1;i<n+n-1;i++){
+            for(int j=n-1;j<n+n-1;j++){
+                grid[i][j]=img1[i-n+1][j-n+1];
+             
+            }
+        }
+        for(int i=0;i<2*n-1;i++){
+            for(int j=0;j<2*n-1;j++){
+                 
+                   int cnt=0;
+                   for(int i1=0;i1<n;i1++){
+                        for(int j1=0;j1<n;j1++){
+                            if(grid[i+i1][j+j1]==img2[i1][j1]){
+                                if(img2[i1][j1]==1) cnt++;
+                            }
+                        }
+                       
+                   }
+                  maxi=max(maxi,cnt);
+            }
+        }
+        return maxi;
+    }
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        return comp(img1,img2);
     }
 };
