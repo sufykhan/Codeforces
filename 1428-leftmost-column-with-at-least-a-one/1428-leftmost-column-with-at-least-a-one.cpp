1000473eeb5295ab2e423fea0536949219a77365
Sufyan <57655074+sufykhan@users.noreply.github.com>
2022-12-30 20:18:09 +0530

    Time: 9 ms (80.47%), Space: 8.1 MB (83.20%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
@@ -12,31 +12,43 @@ class Solution {
 public:
     int leftMostColumnWithOne(BinaryMatrix &b) {
         auto dim=b.dimensions();
-        int row=dim[0],col=dim[1];
+        int row=0,col=dim[1]-1;
         
-        int curr=col;
-        for(int i=0;i<row;i++){
-            
-            int l=0,r=curr-1;
-            
-            while(r>=l){
-                int mid=l+(r-l)/2;
-                if(b.get(i,mid)==1){
-                    r=mid-1;
-                }
-                else{
-                    l=mid+1;
-                }
+        while(row<dim[0] && col>=0 ){
+            if(b.get(row,col)==1){
+                col--;
             }
-            
-            if(l!=curr){
-                curr=l;
+            else{
+                row++;
             }
-
         }
+        if(col+1==dim[1]) return -1;
+        return col+1;
+//         int curr=col;
+//         for(int i=0;i<row;i++){
+            
+//             int l=0,r=curr-1;
+            
+//             while(r>=l){
+//                 int mid=l+(r-l)/2;
+//                 if(b.get(i,mid)==1){
+//                     r=mid-1;
+//                 }
+//                 else{
+//                     l=mid+1;
+//                 }
+//             }
+            
+//             if(l!=curr){
+//                 curr=l;
+//             }
+
+//         }
         
-        if(curr==col) return -1;
-        else return curr;
+//         if(curr==col) return -1;
+//         else return curr;
+        
+     
         
     }
 };
