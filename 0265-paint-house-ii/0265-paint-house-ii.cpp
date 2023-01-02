23fe1a11c2655f902f967e09de7554d788827199
Sufyan <57655074+sufykhan@users.noreply.github.com>
2023-01-03 00:31:59 +0530

    Time: 26 ms (59.55%), Space: 11.8 MB (22.19%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.cpp
+++ 0265-paint-house-ii/0265-paint-house-ii.cpp
@@ -5,12 +5,17 @@ public:
         int m=cost[0].size();
         vector<vector<int>>dp(n+1,vector<int>(m,0));
         for(int i=n-1;i>=0;i--){
+            int fmin=INT_MAX,smin=INT_MAX;
+            int pos=-1;
             for(int j=0;j<m;j++){
-                int mini=INT_MAX;
-                for(int k=0;k<m;k++){
-                    if(k!=j) mini=min(mini,cost[i][j]+dp[i+1][k]);
-                }
-                dp[i][j]=mini;
+               if(fmin>dp[i+1][j]) smin=fmin,fmin=dp[i+1][j],pos=j;
+               else if(smin>dp[i+1][j]) smin=dp[i+1][j];
+            }
+            for(int j=0;j<m;j++){
+                
+                if(pos!=j) dp[i][j]=cost[i][j]+fmin;
+                else dp[i][j]=cost[i][j]+smin;
+                
             }
             
         }
