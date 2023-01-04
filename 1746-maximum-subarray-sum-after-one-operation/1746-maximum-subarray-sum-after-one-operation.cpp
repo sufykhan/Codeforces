288ed1d88be651dcb4e5998fc9ecfceebd9f13e5
Sufyan <57655074+sufykhan@users.noreply.github.com>
2023-01-05 04:27:08 +0530

    Time: 133 ms (91.38%), Space: 68.1 MB (91.38%) - LeetHub

--- 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -1,32 +1,12 @@
 class Solution {
 public:
-    vector<long long> give(vector<int>&nums,int n){
-        vector<long long>pref(n+1,0);
-        vector<long long>mini(n+1,0);
-        vector<long long>maxend(n+1,0);
-        for(int i=0;i<n;i++){
-            pref[i+1]=pref[i]+nums[i];
-            mini[i+1]=min({mini[i],pref[i],pref[i+1]});
-            maxend[i+1]=pref[i+1]-mini[i+1];
-        }
-        return maxend;
-    }
     int maxSumAfterOperation(vector<int>& nums) {
-        
-        int n=nums.size();
-        
-        vector<long long>maxEndleft=give(nums,n);
-        reverse(nums.begin(),nums.end());
-        vector<long long>maxEndright=give(nums,n);
-         reverse(nums.begin(),nums.end());
-        long long ans=LONG_MIN;
-        for(int i=0;i<n;i++){
-            long long val=nums[i]*1LL*nums[i];
-            val=val+max(0LL,maxEndleft[i])+max(0LL,maxEndright[n-i-1]);
-            ans=max(ans,val);
+        int sum_uptil_prev = 0, maxi = 0, ans = 0;
+        for (auto &x : nums) {
+            maxi = max(maxi + x, sum_uptil_prev + x * x);
+            sum_uptil_prev = max(0, x + sum_uptil_prev);
+            ans = max(ans, maxi);
         }
-        
         return ans;
-        
     }
 };
